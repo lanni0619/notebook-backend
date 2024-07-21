ddcca6db046ae5b99626b9da06b83891f2367254
RoyWang <66111562+lanni0619@users.noreply.github.com>
2024-07-21 22:38:29 +0800

    Oauth
    modify callbackURL

--- config/passport-google.js
+++ config/passport-google.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      callbackURL: "/auth/google/redirect",
+      callbackURL: process.env.SERVER_URL + "/auth/google/redirect",
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log("===Googel Strategy zone===");
